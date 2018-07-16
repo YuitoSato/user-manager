13c6b123ce6c4845ffa1cff88664be2c999af1ac
Yuito <yuitosato.w@gmail.com>
2018-07-17 00:19:02 +0900

    : )

--- modules/interface/app/syntax/ToResultOps.scala
+++ modules/interface/app/syntax/ToResultOps.scala
@@ -19,26 +19,24 @@ trait ToResultOps extends Results with ToFutureOps with ToEitherOps {
   val logger = Logger(this.getClass)
 
   implicit class ErrorToOps(error: Error) {
-    def toResult: mvc.Result = error match {
-      case InterfaceError.SessionKeyNotFound => BadRequest(toJson)
-      case _: InterfaceError.JsonReadsError  => BadRequest(toJson)
-      case DomainError.Unauthorized          => Unauthorized(toJson)
-      case _: ApplicationError.NotFound      => NotFound(toJson)
-      case ApplicationError.EmailExists      => Conflict(toJson)
-      case ApplicationError.EmailNotFound    => Unauthorized(toJson)
-      case _                                 => InternalServerError(toJson)
+    def toResult: mvc.Result = {
+      if (error.logging) logger.error(error.toString)
+
+      error match {
+        case InterfaceError.SessionKeyNotFound => BadRequest(toJson)
+        case _: InterfaceError.JsonReadsError => BadRequest(toJson)
+        case DomainError.Unauthorized => Unauthorized(toJson)
+        case _: ApplicationError.NotFound => NotFound(toJson)
+        case ApplicationError.EmailExists => Conflict(toJson)
+        case ApplicationError.EmailNotFound => Unauthorized(toJson)
+        case _ => InternalServerError(toJson)
+      }
     }
 
     private def toJson: JsObject = Json.obj(
       "code" -> error.code,
       "message" -> error.message
     )
-  }
-
-  implicit class EitherTToResultOps[A](either: EitherT[Future, Error, A]) {
-
-    implicit def to(implicit ec: ExecutionContext): AsyncResult[A] = AsyncResult(either)
-
   }
 
   implicit class ResultToResultOpts[A](result: Result[A]) {
@@ -49,7 +47,7 @@ trait ToResultOps extends Results with ToFutureOps with ToEitherOps {
       }
     }
 
-    def handleErrorAsync(fun: A => mvc.Result)(implicit ec: ExecutionContext): Future[mvc.Result] = {
+    private def handleErrorAsync(fun: A => mvc.Result)(implicit ec: ExecutionContext): Future[mvc.Result] = {
       val asyncResult: AsyncResult[A] = result match {
         case sync: SyncResult[A] => AsyncResult(sync.value.future.et)
         case async: AsyncResult[A] => async
@@ -72,7 +70,7 @@ trait ToResultOps extends Results with ToFutureOps with ToEitherOps {
       }
     }
 
-    def handleErrorSync(fun: A => mvc.Result): mvc.Result = {
+    private def handleErrorSync(fun: A => mvc.Result): mvc.Result = {
       val syncResult: SyncResult[A] = result match {
         case sync: SyncResult[A] => sync
         case async: AsyncResult[A] => SyncResult(Await.result(async.value.run, Duration.Inf))
